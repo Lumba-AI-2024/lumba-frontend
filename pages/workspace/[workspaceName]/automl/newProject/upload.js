33dc688240c84d87285d111fae40e55aca5dd13a
Mohammad Bryan Mahdavikhia <mohammad.bryan@ui.ac.id>
2024-05-11 00:16:14 +0700

    update upload auto ml page

--- pages/workspace/[workspaceName]/automl/newProject/upload.js
+++ pages/workspace/[workspaceName]/automl/newProject/upload.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useRouter } from "next/router";
 import Breadcrumb from "../../../../../src/components/Breadcrumb";
 import FormModalContextProvider from "../../../../../src/context/FormModalContext";
@@ -11,14 +11,20 @@ import { useSearchParams } from "next/navigation";
 import Seo from "../../../../../src/components/Seo";
 import Button from "../../../../../src/components/Button/Button";
 import SidebarAuto from "../../../../../src/components/SidebarAuto";
-import DatasetPage from "../../../../../src/components/Tutorial/Dataset/DatasetPage";
+import FormModal from "../../../../../src/components/Form/FormModal";
+import HiddenInput from "../../../../../src/components/Form/HiddenInput";
+import Input from "../../../../../src/components/Form/Input";
+import Select from "../../../../../src/components/Select/Select";
+import * as React from "react";
+import { FormModalContext } from "../../../../../src/context/FormModalContext";
 
 
 const upload = () => {
-    
-    const router = useRouter();
 
-    let searchParams = useSearchParams();   
+    const router = useRouter();
+    const { formData } = React.useContext(FormModalContext);
+
+    let searchParams = useSearchParams();
     let type = searchParams.get("type");
 
     const { workspaceName } = router.query;
@@ -26,30 +32,74 @@ const upload = () => {
     const username = useCookie("username");
 
     const { datasets, addDataset } = useDatasets(workspaceName, username, type);
-    const [isUploading, setIsUploading] = useState(false); 
+    const [isUploading, setIsUploading] = useState(false);
+
+    const [isChecked, setIsChecked] = useState(false);
+    const [dataset, setDataset] = useState(formData?.dataset);
+    
 
     const back = () => {
         router.push(`/workspace/${workspaceName}/automl?type=${type}`);
     };
 
+    const next = () => {
+        router.push(`/workspace/${workspaceName}/automl/newProject/preprocess?type=${type}&checkedDataset=${dataset}`);
+    };
+
+    React.useEffect(() => {
+        if (formData?.dataset) {
+            setDataset(formData?.dataset);
+            // setIsChecked(true);
+            console.log(formData?.dataset); // Log the dataset if it changes
+        }
+    }, [formData]); 
 
     return (
         <>
-        <Seo title={`${workspaceName} - AutoML`} />
+            <Seo title={`${workspaceName} - AutoML`} />
             <div className="flex">
                 <SidebarAuto />
                 <div className="flex-1 p-8">  {/* Adjusted padding and flex-grow */}
                     <Breadcrumb links={[
-                        { label: workspaceName }, 
+                        { label: workspaceName },
                         { label: "AutoML", href: "/workspace/" + workspaceName + "/automl" },
                         { label: "new project", href: router.asPath }
                     ]} active={"AutoML"} />
                     <div className="flex flex-col gap-6 my-6">
                         <h1>Create a new automated ML Job</h1>
                     </div>
-                    <div className="h-full flex flex-col">
-                        <div className="flex items-center">
-                            <FormModalContextProvider>
+                    <div className="vertical-form-container">
+                        <HiddenInput name="username" defaultValue={username} />
+
+                        <div className="form-field">
+                            <Input label={"Name"} name={"name"} placeholder="AutoML job name" required />
+                        </div>
+
+                        <div className="form-field">
+                            <Input
+                                label={"Description"}
+                                name={"description"}
+                                placeholder="Write description here"
+                                textarea={true}
+                                required
+                            />
+                        </div>
+
+                        <div className="form-field">
+                            <p className="form-label">Will Be Used For</p>
+                            <Select
+                                name="autoMlType"
+                                placeholder="- Select -"
+                                items={[
+                                    { label: "Classification", value: "classification" },
+                                    { label: "Regression", value: "regression" },
+                                    { label: "Clustering", value: "clustering" },
+                                ]}
+                            />
+                        </div>
+                        <h4 className="mt-4">Please choose one either to upload or select dataset</h4>
+                        <FormModalContextProvider>
+                            <div className="mt-4 form-field upload-field">
                                 <UploadFile
                                     buttonLabel={
                                         <div className="flex font-semibold items-center gap-1">
@@ -58,35 +108,55 @@ const upload = () => {
                                         </div>
                                     }
                                     formLabel="Upload File Datasets"
-                                    handleSubmit={ async (formData) => {
+                                    handleSubmit={async (formData) => {
                                         setIsUploading(true);
                                         const dataset = new FormData();
                                         dataset.append("file", formData?.file);
-                                        console.log(formData?.file);
                                         dataset.append("username", username);
                                         dataset.append("workspace", workspaceName);
                                         dataset.append("type", type);
                                         try {
-                                          // Wait for the addDataset function to complete
-                                          await addDataset(dataset);
-                                          // If successful, reset the uploading state and navigate
-                                          setIsUploading(false);
-                                          router.push(`/workspace/${workspaceName}/automl/newProject/preprocess?type=${type}`);
+                                            await addDataset(dataset);
+                                            setIsUploading(false);
+                                            setIsChecked(true);
+                                            setDataset(formData?.file["name"]);
+                                            console.log(formData?.file["name"]);
+                                            // router.push(`/workspace/${workspaceName}/automl/newProject/preprocess?type=${type}`);
                                         } catch (error) {
-                                            // Handle errors if the upload fails
                                             console.error('Upload failed:', error);
                                             setIsUploading(false);
-                                            // Optionally, set error messages or state here
                                         }
                                     }}
                                     workspaceType={type}
                                 />
-                            </FormModalContextProvider>
+                            </div>
+                        </FormModalContextProvider>
+                        <div className="mt-4 flex gap-3 items-center">
+                            <div className="flex flex-col gap-1 w-full">
+                                <span className="font-semibold">Select Dataset</span>
+                                <Select
+                                    isDisabled={isChecked}
+                                    variant="withoutBorder"
+                                    placeholder="Select dataset..."
+                                    name="dataset"
+                                    items={datasets?.map((dataset) => ({ value: dataset.name, label: dataset.name })) || []}
+                                />
+                            </div>
                         </div>
-                        <div className="flex items-center my-6">
+                    </div>
+
+                    <div className="flex items-center my-6">
+                        <div className="mr-4 flex items-center gap-1">
                             <Button onClick={back} className="flex items-center gap-1">
                                 <div className="flex font-semibold items-center gap-1">
                                     Back
+                                </div>
+                            </Button>
+                        </div>
+                        <div className="flex items-center gap-1">
+                            <Button onClick={next} className="flex items-center gap-1">
+                                <div className="flex font-semibold items-center gap-1">
+                                    Next
                                 </div>
                             </Button>
                         </div>
