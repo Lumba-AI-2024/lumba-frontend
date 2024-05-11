6ddf742e00036fd37a2ddbfbac53c46aa405ba63
Mohammad Bryan Mahdavikhia <mohammad.bryan@ui.ac.id>
2024-05-12 00:30:42 +0700

    fix sidebar automl

--- src/components/SidebarAuto.js
+++ src/components/SidebarAuto.js
@@ -11,28 +11,42 @@ import useCookie from "../hooks/useCookie";
 
 
 const steps = [
-  { label: "Select Dataset" },
-  { label: "Preprocessing Check" },
-  { label: "Select Target"},
+  { label: "Select Dataset", href: "upload" },
+  { label: "Preprocessing Check", href: "preprocess" },
 ];
 
 
 export default function SidebarAuto() {
-    const [activeStep, setActiveStep] = useState(0);  // State to keep track of the active step
+  const router = useRouter();
+  const { workspaceName } = router.query;
 
-    return (
-      <div className="mr-7 relative py-4 pl-8 h-[calc(200vh-55px)] bg-white shadow pr-4">
-        <ul className="flex flex-col gap-8 py-4">
-          {steps.map((step, index) => (
-            <li key={index} className={`flex items-center font-medium cursor-pointer transition duration-300 ${index === activeStep ? 'text-green-500' : 'hover:text-gray-500'}`}
-              onClick={() => setActiveStep(index)}>
-              {index + 1}. {step.label}
-            </li>
-          ))}
-        </ul>
-        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-20">
-          <img src="/assets/LumbaSidebar.svg" alt="Lumba" />
-        </div>
+  // Get the current pathname from the router
+  const currentPathname = router.pathname;
+
+
+
+  return (
+    <div className="mr-7 relative py-4 pl-8 h-[calc(200vh-55px)] bg-white shadow pr-4">
+      <ul className="flex flex-col gap-8 py-4">
+        {steps.map((step, index) => (
+          
+        <li
+          key={index}
+          className={`flex items-center font-medium transition duration-300 ${currentPathname.startsWith(`/workspace/[workspaceName]/automl/newProject/${step.href}`) ? 'text-green-500' : 'hover:text-gray-500'}`}
+        >
+          
+          {console.log(currentPathname)}
+          {console.log(workspaceName)}
+          {console.log(step.href)}
+          {console.log(`/workspace/${workspaceName}/automl/newProject${step.href}`)}
+          {console.log(currentPathname === `/workspace/[workspaceName]/automl/newProject${step.href}`)}
+          {index + 1}. {step.label}
+        </li>
+        ))}
+      </ul>
+      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-20">
+        <img src="/assets/LumbaSidebar.svg" alt="Lumba" />
       </div>
-    );
+    </div>
+  );
 }
